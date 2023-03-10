84e6e7a7dfa704b883463724e3f7a71595721481
jk <1875809993@qq.com>
2023-03-10 11:13:48 +0800

    a

--- fgoTTScript.py
+++ fgoTTScript.py
@@ -2,7 +2,7 @@
 Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
 Date: 2023-02-28 17:59:27
 LastEditors: jk 1875809993@qq.com
-LastEditTime: 2023-03-09 17:42:48
+LastEditTime: 2023-03-09 21:00:33
 FilePath: \projectp\set_win.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
