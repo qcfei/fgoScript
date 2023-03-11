1953e9f3da5cc9589179c6b44e29a29e1313aea3
jk <1875809993@qq.com>
2023-03-11 21:20:43 +0800

    miniInstall

---
+++ minitouch/x86_64/objs/evdev/source/libevdev/libevdev.o.d
@@ -0,0 +1,23 @@
+./obj/local/x86_64/objs/evdev/source/libevdev/libevdev.o: \
+  D:/git/Git/minitouch/jni/vendor/libevdev/source/libevdev/libevdev.c \
+  D:/git/Git/minitouch/jni/vendor/libevdev/include/config.h \
+  D:/git/Git/minitouch/jni/vendor/libevdev/source/libevdev/libevdev.h \
+  D:/git/Git/minitouch/jni/vendor/libevdev/source/include/linux/input.h \
+  D:/git/Git/minitouch/jni/vendor/libevdev/source/include/linux/input-event-codes.h \
+  D:/git/Git/minitouch/jni/vendor/libevdev/source/libevdev/libevdev-int.h \
+  D:/git/Git/minitouch/jni/vendor/libevdev/source/libevdev/libevdev-util.h \
+  D:/git/Git/minitouch/jni/vendor/libevdev/include/event-names.h
+
+D:/git/Git/minitouch/jni/vendor/libevdev/include/config.h:
+
+D:/git/Git/minitouch/jni/vendor/libevdev/source/libevdev/libevdev.h:
+
+D:/git/Git/minitouch/jni/vendor/libevdev/source/include/linux/input.h:
+
+D:/git/Git/minitouch/jni/vendor/libevdev/source/include/linux/input-event-codes.h:
+
+D:/git/Git/minitouch/jni/vendor/libevdev/source/libevdev/libevdev-int.h:
+
+D:/git/Git/minitouch/jni/vendor/libevdev/source/libevdev/libevdev-util.h:
+
+D:/git/Git/minitouch/jni/vendor/libevdev/include/event-names.h:
